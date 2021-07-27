ba24bf315d6ad9d52c50108d3fac0fb59d6b44b8
evg226 <evg226@hotmail.com>
2021-07-27 17:28:54 +0500

    Исправлено мелких ошибок по роботу и form-input
     1-блок отправки нового сообщения до второго ответа робота и пустых сообщений, 2-устранены ошибки сообщений робота по ключу

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ const currentAuthor = "Evgeny";
 
 function App() {
     const [messageList, setMessageList] = useState([]);
+    const [isInput, setIsInput] = useState(true);
 
     const addNewMessage = (newMessage) => {
+        setIsInput(() => false);
         setMessageList(prevMessageList => [...prevMessageList, newMessage]);
     }
 
-    const robotSay = (robotText) => {
-        addNewMessage({ id: new Date(), author: "Robot", text: robotText });
+    const robotSay = (robotText,key) => {
+        addNewMessage({ id: key+new Date(), author: "Robot", text: robotText });
     }
 
     let robotProcessedTimeout;
 
     useEffect(() => {
         if (messageList.length > 0 && messageList[messageList.length - 1].author === currentAuthor) {
-            robotSay(`${currentAuthor}, от Вас получено сообщение "${messageList[messageList.length - 1].text}"`);
+            robotSay(`${currentAuthor}, от Вас получено сообщение "${messageList[messageList.length - 1].text}"`,"accept");
             robotProcessedTimeout = setTimeout(() => {
-                robotSay(`${currentAuthor}, обработано Ваше сообщение "${messageList[messageList.length - 1].text}"`);
+                robotSay(`${currentAuthor}, обработано Ваше сообщение "${messageList[messageList.length - 1].text}"`,"process");
+                setIsInput(() => true);
             }, 1500);
         }
     }, [messageList]);
@@ -38,7 +41,7 @@ function App() {
             <div className = "messenger__container">
                     <h1 className="messenger__header" > Мессенджер </h1>
                     <MessageInputForm classList = "messenger__input"
-                        author={currentAuthor} addMessage={addNewMessage} />
+                        author={currentAuthor} isInput={isInput} addMessage={addNewMessage} />
                     <div className="messenger__items">
                         <h2 className="messenger__items-header"> Сообщения </h2>
                         {messageList.map(message => < Message key={message.id} mes={message} />)}
--- src/MessageInputForm.js
+++ src/MessageInputForm.js
@@ -8,14 +8,15 @@ function MessageInputForm(props) {
 
   const sendNewMessage = e => {
     e.preventDefault();
+    if (!currentMessageText) return;
     props.addMessage({ id: props.author + new Date(), author: props.author, text: currentMessageText });
     setCurrentMessageText("");
   };
 
   return (
     <form name="newMessage" onSubmit={sendNewMessage}>
-      <input className="text" type="text" value={currentMessageText} onChange={newMessgeChange}></input>
-      <input className="button" type="submit" value="Отправить"></input>
+      <input className="text" type="text" value={currentMessageText} onChange={newMessgeChange} disabled={!props.isInput}></input>
+      <input className="button" type="submit" value="Отправить" disabled={!props.isInput}></input>
     </form>
   )
 }
