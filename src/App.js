334226cc22cdd2c5b40622bba7868919e756759d
evg226 <evg226@hotmail.com>
2021-07-27 15:39:47 +0500

    Устранены замечания

--- src/App.js
+++ src/App.js
@@ -4,40 +4,49 @@ import './App.scss';
 import MessageInputForm from "./MessageInputForm"
 import Message from "./Message"
 
+const currentAuthor = "Evgeny";
+
 function App() {
-  let currentAuthor = "Evgeny";
-  const [messageList, setMessageList] = useState([]);
+    const [messageList, setMessageList] = useState([]);
 
-  useEffect(() => {
-    if (messageList.length > 0 && messageList[messageList.length - 1].author == currentAuthor) {
-      const robotSay = (robotText) => {
-        addNewMessage({ id: new Date(), author: "Robot", text: robotText });
-      }
-      robotSay(`${currentAuthor}, от Вас получено сообщение "${messageList[messageList.length - 1].text}"`);
-      setTimeout(() => {
-        robotSay(`${currentAuthor}, обработано Ваше сообщение "${messageList[messageList.length - 1].text}"`);
-      }, 1500);
+    const addNewMessage = (newMessage) => {
+        setMessageList(prevMessageList => [...prevMessageList, newMessage]);
     }
-  }, [messageList]);
 
-  const addNewMessage = (newMessage) => {
-    setMessageList(prevMessageList => [...prevMessageList, newMessage]);
-  }
+    const robotSay = (robotText) => {
+        addNewMessage({ id: new Date(), author: "Robot", text: robotText });
+    }
 
-  return (
-    <div className="container">
-      <div className="messenger" >
-        <div className="messenger__container">
-          <h1 className="messenger__header">Мессенджер</h1>
-          <MessageInputForm classList="messenger__input" author={currentAuthor} addMessage={addNewMessage} />
-          <div className="messenger__items">
-            <h2 className="messenger__items-header">Сообщения</h2>
-            {messageList.map(message => <Message key={message.id} mes={message} />)}
-          </div>
+    let robotProcessedTimeout;
+
+    useEffect(() => {
+        if (messageList.length > 0 && messageList[messageList.length - 1].author === currentAuthor) {
+            robotSay(`${currentAuthor}, от Вас получено сообщение "${messageList[messageList.length - 1].text}"`);
+            robotProcessedTimeout = setTimeout(() => {
+                robotSay(`${currentAuthor}, обработано Ваше сообщение "${messageList[messageList.length - 1].text}"`);
+            }, 1500);
+        }
+    }, [messageList]);
+
+    useEffect(() => {
+        return () => robotProcessedTimeout && clearTimeout(robotProcessedTimeout);
+    }, []);
+
+    return (
+        <div className = "container">
+        <div className = "messenger">
+            <div className = "messenger__container">
+                    <h1 className="messenger__header" > Мессенджер </h1>
+                    <MessageInputForm classList = "messenger__input"
+                        author={currentAuthor} addMessage={addNewMessage} />
+                    <div className="messenger__items">
+                        <h2 className="messenger__items-header"> Сообщения </h2>
+                        {messageList.map(message => < Message key={message.id} mes={message} />)}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div >
-    </div >
-  );
+    );
 }
 
 export default App;
